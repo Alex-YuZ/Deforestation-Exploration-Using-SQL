ae1da32d790cd6ef1031cc6bddd8acdaf496173b
Alex-Yu <alexyuzhu@gmail.com>
2021-06-25 13:09:21 +0800

    feat: Add calculation on finding the country with its land area in 2016 closest to the deforestation area between 1990 and 2016

--- forestation.sql
+++ forestation.sql
@@ -37,4 +37,23 @@ FROM t1
 
 
 
-
+-- Find the country with its land area in 2016 closest to the deforestation area between 1990 and 2016
+WITH t1 AS (
+  SELECT *
+  FROM forestation
+  WHERE country_code='WLD' and year in (1990, 2016)
+  ORDER BY year), 
+  t2 AS (
+    SELECT year,
+      forest_area_sqkm,
+      LEAD(forest_area_sqkm) OVER (order by year) AS lead,
+      LEAD(forest_area_sqkm) OVER (order by year)-forest_area_sqkm AS abs_diff,
+      ROUND((100*(LEAD(forest_area_sqkm) OVER (order by year)-forest_area_sqkm)/forest_area_sqkm)::NUMERIC, 3) AS pct_diff
+    FROM t1)
+  
+SELECT DISTINCT country_name,
+                total_area_sq_km, 
+                (SELECT ABS(t2.abs_diff) FROM t2 ORDER BY year LIMIT 1) AS abs_diff,
+                ABS(total_area_sq_km-(SELECT ABS(t2.abs_diff) FROM t2 ORDER BY year LIMIT 1)) AS diff
+FROM forestation
+ORDER BY 4
