35713b833c990885c4769493aef146c08e6c92be
Alex-Yu <alexyuzhu@gmail.com>
2021-06-25 13:20:08 +0800

    feat: Add query on finding the world's forestation area percentage in 2016

--- forestation.sql
+++ forestation.sql
@@ -20,7 +20,7 @@ select count(*)
 from forestation;
 
 
--- PARTI. GLOBAL SITUATION
+-- PART I. GLOBAL SITUATION
 -- Difference and percentage drop in forestation area from 1990 to 2016
 WITH t1 AS (
   SELECT *
@@ -57,3 +57,16 @@ SELECT DISTINCT country_name,
                 ABS(total_area_sq_km-(SELECT ABS(t2.abs_diff) FROM t2 ORDER BY year LIMIT 1)) AS diff
 FROM forestation
 ORDER BY 4
+
+-- Part II. Regional Outlook
+-- Find the world's forestation area percentage in 2016
+SELECT country_code,
+       country_name,
+       year,
+       forest_area_sqkm,
+       total_area_sq_km,
+       ROUND(pct_forestation::NUMERIC, 2) pct_forestation
+FROM forestation
+WHERE year=2016 AND country_code='WLD';
+
+-- Find the region with the highest relative forestation
